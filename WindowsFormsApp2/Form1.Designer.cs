cfbc1a3a8c2713bec30770cd4c3f48340b741514
Ohm Patel <ohmpatel86@ed.amdsb.ca>
2018-10-29 14:54:15 -0400

    started

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -29,22 +29,84 @@
         private void InitializeComponent()
         {
             this.outputLabel = new System.Windows.Forms.Label();
+            this.bluebuttonLabel = new System.Windows.Forms.Label();
+            this.redbuttonLabel = new System.Windows.Forms.Label();
+            this.greenbuttonLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // outputLabel
             // 
-            this.outputLabel.AutoSize = true;
-            this.outputLabel.Location = new System.Drawing.Point(183, 112);
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(35, 13);
+            this.outputLabel.Size = new System.Drawing.Size(454, 257);
             this.outputLabel.TabIndex = 0;
             this.outputLabel.Text = "label1";
+            // 
+            // bluebuttonLabel
+            // 
+            this.bluebuttonLabel.Image = global::WindowsFormsApp2.Properties.Resources.blue_50x50;
+            this.bluebuttonLabel.Location = new System.Drawing.Point(12, 294);
+            this.bluebuttonLabel.Name = "bluebuttonLabel";
+            this.bluebuttonLabel.Size = new System.Drawing.Size(100, 51);
+            this.bluebuttonLabel.TabIndex = 1;
+            // 
+            // redbuttonLabel
+            // 
+            this.redbuttonLabel.Image = global::WindowsFormsApp2.Properties.Resources.red_50x50;
+            this.redbuttonLabel.Location = new System.Drawing.Point(12, 345);
+            this.redbuttonLabel.Name = "redbuttonLabel";
+            this.redbuttonLabel.Size = new System.Drawing.Size(100, 49);
+            this.redbuttonLabel.TabIndex = 2;
+            // 
+            // greenbuttonLabel
+            // 
+            this.greenbuttonLabel.Image = global::WindowsFormsApp2.Properties.Resources.green_50x50;
+            this.greenbuttonLabel.Location = new System.Drawing.Point(12, 394);
+            this.greenbuttonLabel.Name = "greenbuttonLabel";
+            this.greenbuttonLabel.Size = new System.Drawing.Size(100, 47);
+            this.greenbuttonLabel.TabIndex = 3;
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.AutoSize = true;
+            this.blueLabel.Location = new System.Drawing.Point(118, 312);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(35, 13);
+            this.blueLabel.TabIndex = 4;
+            this.blueLabel.Text = "label1";
+            // 
+            // redLabel
+            // 
+            this.redLabel.AutoSize = true;
+            this.redLabel.Location = new System.Drawing.Point(118, 363);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(35, 13);
+            this.redLabel.TabIndex = 5;
+            this.redLabel.Text = "label2";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.AutoSize = true;
+            this.greenLabel.Location = new System.Drawing.Point(118, 412);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(35, 13);
+            this.greenLabel.TabIndex = 6;
+            this.greenLabel.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(746, 450);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.greenbuttonLabel);
+            this.Controls.Add(this.redbuttonLabel);
+            this.Controls.Add(this.bluebuttonLabel);
             this.Controls.Add(this.outputLabel);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +118,12 @@
         #endregion
 
         private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label bluebuttonLabel;
+        private System.Windows.Forms.Label redbuttonLabel;
+        private System.Windows.Forms.Label greenbuttonLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label greenLabel;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -12,9 +12,146 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        //Scene number and track where person is at
+        int scene = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0) 
+                {
+                    scene = 1;
+                }
+
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+
+            }
+            else if (e.KeyCode == Keys.Space) //green button press
+            {
+
+            }
+
+            //shows options and text based on what scene you are on
+            switch (scene)
+            {
+                case 0:  //start scene 
+                    outputLabel.Text = "It is Friday the 13th of October, 1980 You decide to go camping with your friends ";
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 11:
+
+                    break;
+                case 12:
+
+                    break;
+                case 13:
+
+                    break;
+                case 14:
+
+                    break;
+                case 15:
+
+                    break;
+                case 16:
+
+                    break;
+                case 17:
+
+                    break;
+                case 18:
+
+                    break;
+                case 19:
+
+                    break;
+                case 20:
+
+                    break;
+                case 21:
+
+                    break;
+                case 22:
+
+                    break;
+                case 23:
+
+                    break;
+                case 24:
+
+                    break;
+                case 25:
+
+                    break;
+                case 26:
+
+                    break;
+                case 27:
+
+                    break;
+                case 28:
+
+                    break;
+                case 29:
+
+                    break;
+                case 30:
+
+                    break;
+                case 31:
+
+                    break;
+                case 32:
+
+                    break;
+                case 33:
+
+                    break;
+                case 34:
+
+                    break;
+
+
+
+            }
         }
     }
 }
